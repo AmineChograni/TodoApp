06f3c065515b6fa81b39d45569fc7a67d3e95d4f
Amine Chograni <aminechograni@gmail.com>
2021-05-17 13:43:51 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityTodo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+    }
+}
---
+++ Data/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityTodo.Data
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Task { get; set; }
+        public bool Complete { get; set; }
+        public string Owner { get; set; }
+    }
+}
